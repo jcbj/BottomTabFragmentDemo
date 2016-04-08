a09329225bbb6d997032d93fe246026fc03a69bf
jiangchao <jiangchaobj@yahoo.com>
2016-04-09 06:37:26 +0800

    测试显示Fragment,能显示出来

--- app/src/main/java/com/example/jc/bottomtabfragmentdemo/MainActivity.java
+++ app/src/main/java/com/example/jc/bottomtabfragmentdemo/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.jc.bottomtabfragmentdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
+
+    public static Fragment[] mFragments;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFragments = new Fragment[2];
+        mFragments[0] = getSupportFragmentManager().findFragmentById(R.id.fragmentHome);
+        mFragments[1] = getSupportFragmentManager().findFragmentById(R.id.fragmentSummary);
+
+        getSupportFragmentManager().beginTransaction().hide(mFragments[0]).show(mFragments[1]).commit();
+
     }
 }
